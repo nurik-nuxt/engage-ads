ca2925068755b19fb347d4df3c60bc5f1fde654e
nurtuganulyn <nurtuganulyn@gmail.com>
2024-08-06 21:06:23 +0500

    fix

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Alert } from 'vue-tg'
+</script>
+
+<template>
+  <main>
+    Main Page
+    <Alert message="Hello Engage Ads Telegram Mini App" />
+  </main>
+</template>
